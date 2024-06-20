81166aafcb907c1c5b1cf7c09c025c8618177aa7
carlosPauli1 <carlos.pauli@outlook.com>
2024-06-19 21:32:03 -0300

    Adicionando primeiros arquivos

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './img/logo.png';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <div className='App-hearder-logo'> 
+          <img src={logo} alt='logo-site'></img>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
